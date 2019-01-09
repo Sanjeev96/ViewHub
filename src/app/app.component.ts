b1e031ed4d91542aa4309d32956e2264b123ac10
Sanjeev96 <sanjeevdhillon12@hotmail.co.uk>
2019-01-09 22:54:35 +0000

    Fixed some Css and passing Search Input to button console log

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,10 +7,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ViewHub';
-
   navLinks = [
-    {path: 'tvShows', label: 'Tv-Shows'},
-    {path: 'home', label: 'Home'},
-    {path: 'movies', label: 'Movies'}
+    { path: 'tvShows', label: 'Tv-Shows' },
+    { path: 'home', label: 'Home' },
+    { path: 'movies', label: 'Movies' }
   ];
+
+  @ViewChild('SearchInput') SearchInput: ElementRef
+
+  constructor() {
+  }
+
+  ngOnInit() {
+
+  }
+  searchBtn() {
+    let search = this.SearchInput.nativeElement.value;
+
+    if (search == null || search == '') {
+      alert('Please Enter Search');
+    } else {
+      console.log(search)
+    }
+  }
 }
