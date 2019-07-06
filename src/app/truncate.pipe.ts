1e28600dc0d529bb12e59350dd6365585e50e2a3
sanjeeve1996 <sanjeev.dhillon@esure.com>
2019-07-06 17:33:18 +0100

    feat: dots for truncate

--- src/app/truncate.pipe.ts
+++ src/app/truncate.pipe.ts
@@ -5,6 +5,6 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class TruncatePipe implements PipeTransform {
   transform(value: any) {
-    return value.substr(0, 150);
+    return value.substr(0, 180) + '...';
   }
 }
